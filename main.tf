fed0e9a326efa20296667719baec714f25c33574
anand944 <anandkumarkumbaji07@gmail.com>
2022-09-20 11:45:43 +0530

    ddjf

---
+++ main.tf
@@ -0,0 +1,22 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+module "ResourceGroup"{
+    source = "./ResourceGroup"
+    base_name = "rg989"
+    location = "east us"
+    
+}
